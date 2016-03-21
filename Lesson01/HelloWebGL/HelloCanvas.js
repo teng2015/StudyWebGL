2e259d5416ccb6e747f76334f742003dead4f39c
ZhangWayne <sxyz481747@163.com>
2016-03-21 23:20:33 +0800

    create HelloWebGL

--- Lesson01/HelloWebGL/HelloCanvas.js
+++ Lesson01/HelloWebGL/HelloCanvas.js
@@ -1,3 +1,15 @@
 /**
  * Created by wayne on 16/3/21.
  */
+function main(){
+    var canvas = document.getElementById("webgl");
+
+    var gl = getWebGLContext(canvas);
+    if(!gl){
+        console.log("获取WebGL上下文失败!");
+        return;
+    }
+
+    gl.clearColor(1,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
